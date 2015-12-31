ebb9ba4596c7a33a0380fcc8ad2ddc8db4d4b3dd
Peter Schmitt <ps131110039@googlemail.com>
2015-12-31 01:08:25 +0100

    Init und erste Funktion fertig

---
+++ flightcalc/backend/ToD_ToC_calculations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace backend
+{
+    public static class ToD_ToC_calculations
+    {
+        /***
+            This function returns the covered ground in miles per minute, depending on IAS.
+        ***/
+        public static int dp_min(int IAS)
+        {
+            return (int)Round((double)IAS / 60.0);
+        }
+
+        /***
+            This function returns the rate of descent in ft/min for a given speed, altitude difference and descent distance
+        ***/
+        public static int calculate_rate_of_descent(int IAS, int alt, int target_alt, int target_distance)
+        {
+            int dalt = alt - target_alt;
+            int dpmin = dp_min(IAS);
+
+            double dt = target_distance / dpmin;
+            double rod = dalt / dt;
+
+            return (int)rod;
+        }
+    }
+}
---
+++ flightcalc/flightcalc/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using backend;
+
+namespace flightcalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int currentIAS = 0;
+            int currentAlt = 0;
+            int targetAlt = 0;
+            int targetDistance = 0;
+            int targetROD = 0;
+
+            bool currentIASset = Int32.TryParse(this.textBox_currentIAS.Text, out currentIAS);
+            bool currentAltset = Int32.TryParse(this.textBox_currentAlt.Text, out currentAlt);
+            bool targetAltset = Int32.TryParse(this.textBox_targetAlt.Text, out targetAlt);
+            bool targetDistanceset = Int32.TryParse(this.textBox_targetDistance.Text, out targetDistance);
+            bool targetRODset = Int32.TryParse(this.textBox_targetRateOfDescent.Text, out targetROD);
+
+            string errorLabelText = "";
+            this.errorLabel.Visibility = Visibility.Hidden;
+
+
+
+            if (currentIASset && currentAltset && targetAltset && targetDistanceset)
+            {
+                this.textBox_resultSpeed.Text = backend.ToD_ToC_calculations.dp_min(currentIAS).ToString();
+                this.textBox_resultROD.Text = backend.ToD_ToC_calculations.calculate_rate_of_descent(currentIAS,currentAlt,targetAlt,targetDistance).ToString();
+                return;
+            }
+
+            errorLabelText = "Incorrect IAS given.";
+            this.errorLabel.Content = errorLabelText;
+            this.errorLabel.Visibility = Visibility.Visible;
+        }
+    }
+}
