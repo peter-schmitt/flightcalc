da8db06a381ca396257d867f910312951e05071d
Peter Schmitt <ps131110039@googlemail.com>
2016-01-02 19:49:58 +0100

    Clear Button works now

--- flightcalc/flightcalc/MainWindow.xaml.cs
+++ flightcalc/flightcalc/MainWindow.xaml.cs
@@ -96,5 +96,41 @@ namespace flightcalc
                 return;
             }
         }
+
+        /***
+            Find all Elements on this window with of a certain type
+        ***/
+        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        /***
+            Reset all textboxes and labels
+        ***/
+        private void button_clear_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (TextBox textbox in FindVisualChildren<TextBox>(this))
+            {
+                textbox.Text = "";
+            }
+
+            this.textBlock_statusBar_ToDToC.Text = "";
+        }
     }
 }
